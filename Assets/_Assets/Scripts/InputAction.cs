327105fa3f488066cb3cb52620615a97f1da7038
trungkin <trungkien.nd03@gmail.com>
2025-08-02 18:13:55 +0700

    Done Input 2 Character

--- Assets/_Assets/Scripts/InputAction.cs
+++ Assets/_Assets/Scripts/InputAction.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Purchasing;
 using UnityEngine;
 
 public class InputAction : MonoBehaviour
@@ -15,9 +17,14 @@ public class InputAction : MonoBehaviour
     public bool isGround;
     public bool isJump;
 
+    [Header("CHECK GROUND")]
     [SerializeField] Transform pointCheck;
     public LayerMask LayerMaskGround;
     public float checkGroundRaidus = 0.2f;
+
+    [Header("FIRE BALL")]
+    [SerializeField] Transform pointFire;
+    [SerializeField] GameObject fireBall;
 
     private Rigidbody2D rb;
     private Animator animator;
@@ -26,11 +33,26 @@ public class InputAction : MonoBehaviour
     private const string NAME_ANI_CROUCH = "isCrouch";
     private const string NAME_ANI_JUMP = "isJump";
     private const string NAME_CHECKGROUND = "isGround";
+    private const string NAME_ANI_ATTACKTJUMP = "isJumpAttack";
+    private const string NAME_ANI_ATTACKMELEE = "isAttackMelee";
+    private const string NAME_ANI_ATTACKRANGE = "isAttackRange";
+    private const string NAME_ANI_DASH = "isDash";
+    private const string NAME_ANI_STRIKE = "isStrike";
+    private const string NAME_ANI_HURT = "isHurt";
+    private const string NAME_CHECKUP = "isUp";
 
+
+    [Header("INPUT KEYCODE")]
     public KeyCode inputMoveLeft = KeyCode.A;
     public KeyCode inputMoveRight = KeyCode.D;
     public KeyCode inputCround = KeyCode.S;
     public KeyCode inputJump = KeyCode.Space;
+    public KeyCode inputAttackMelee = KeyCode.J;
+    public KeyCode inputAttackRange = KeyCode.K;
+    public KeyCode inputDash = KeyCode.N;
+    public KeyCode inputStrike = KeyCode.H;
+    public KeyCode inputHurt = KeyCode.I;
+    public KeyCode inputUp = KeyCode.U;
 
     private void Start()
     {
@@ -44,6 +66,12 @@ public class InputAction : MonoBehaviour
         HandleMovement();
         HandleCrounch();
         HandleJump();
+        HandleAttackMelee();
+        HandleAttackRange();
+        HandleDash();
+        HandleStrike();
+        HandleHurt();
+
         CheckGround();
        
     }
@@ -119,7 +147,71 @@ public class InputAction : MonoBehaviour
             animator.SetTrigger(NAME_ANI_JUMP);
             rb.velocity = new Vector2(rb.velocity.x, jumForce);
         }
+
+        if (Input.GetKeyDown(inputAttackMelee) && !isGround)
+        {
+            animator.SetTrigger(NAME_ANI_ATTACKTJUMP);
+        }
     }
 
+    private void HandleAttackMelee()
+    {
+        if(Input.GetKeyDown(inputAttackMelee) && isGround)
+        {
+            animator.SetTrigger(NAME_ANI_ATTACKMELEE);
+        }
+       
+    }
+
+    private void HandleAttackRange()
+    {
+        if (Input.GetKeyDown(inputAttackRange))
+        {
+            animator.SetTrigger(NAME_ANI_ATTACKRANGE);
+            Fire();
+        }    
+        
+    }
+
+    private void HandleDash()
+    {
+        if(Input.GetKeyDown(inputDash))
+        {
+            animator.SetTrigger(NAME_ANI_DASH);
+            Vector3 direction = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+            rb.AddForce(direction * addForceDash, ForceMode2D.Impulse);
+        }
+    }
+
+    private void HandleStrike()
+    {
+        if (Input.GetKeyDown(inputStrike))
+        {
+            animator.SetTrigger(NAME_ANI_STRIKE);
+            Vector3 direction = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+            rb.AddForce(direction * addForceDash, ForceMode2D.Impulse);
+        }
+    }
+
+    private void HandleHurt()
+    {
+        if (Input.GetKeyDown(inputHurt))
+        {
+            animator.SetTrigger(NAME_ANI_HURT);
+            animator.SetBool(NAME_CHECKUP, false);
+        }
+
+        if(Input.GetKeyDown(inputUp))
+        {
+            animator.SetBool(NAME_CHECKUP, true);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject Ball = Instantiate(fireBall, pointFire.position, Quaternion.identity);
+        Vector2 direction = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+        Ball.GetComponent<FireBall>().SetDirection(direction);
+    }
 
 }
